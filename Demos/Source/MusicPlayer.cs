1dcea23c2bdc44ac9c50081db3fc2fac0bf4c6fa
Raoul Marais <raoulmarais@gmail.com>
2019-12-13 09:10:21 +0200

    Test game source code. A Godot C# project using 3.2 beta2.

---
+++ Demos/Source/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Godot;
+using System.Collections.Generic;
+
+namespace Satyrs_Playpark
+{
+
+    public class MusicPlayer
+    {
+        private Dictionary<string, string> musicPlayList = new Dictionary<string, string>();
+
+        public MusicPlayer()
+        {
+            //musicPlayList = new Dictionary<string, string>();
+        }
+
+        public void addSong(string songDescriptor)
+        {
+
+        }
+    }
+}
